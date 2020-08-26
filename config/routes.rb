5aa2e2859f8b6fb366b88e432889f919e6595582
vladislavryabov <ryabov.it@gmail.com>
2020-08-26 17:36:02 +0300

    add

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
+
+  resources :articles do
+  	resources :comments
+  end	
+
+  root 'welcome#index'
+end
